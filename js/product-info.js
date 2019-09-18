ffccfdd9a8b4a152b4a350aa87f45e9261ec47d8
LMarfetan <54559223+LMarfetan@users.noreply.github.com>
2019-09-18 20:47:06 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -1,7 +1,30 @@
 var product = {};
 
-function showImagesGallery(array){
+function firstSlide(array){
+    let htmlContentToAppend = "";
+    let imageSrc = array[0];
 
+        htmlContentToAppend += `
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="` + imageSrc + `" alt="">
+        </div>
+        `
+        document.getElementById("firstSlide").innerHTML = htmlContentToAppend;
+}
+
+function otherSlides(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 1; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="carousel-item">
+            <img class="d-block w-100" src="` + imageSrc + `" alt="">
+        </div>
+        `
+        document.getElementById("otherSlides").innerHTML = htmlContentToAppend;
+    }
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -12,8 +35,22 @@ document.addEventListener("DOMContentLoaded", function(e){
         if (resultObj.status === "ok")
         {
             product = resultObj.data;
-            product = JSON.parse(resultObj.data);
-            showImagesGallery(product);
+            
+
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productSoldCountHTML = document.getElementById("productSoldCount");
+            let productCostHTML = document.getElementById("productCost");
+            let productCurrencyHTML = document.getElementById("productCurrency");
+
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productSoldCountHTML.innerHTML = product.soldCount;
+            productCostHTML.innerHTML = product.cost;
+            productCurrencyHTML.innerHTML = product.currency;
+
+            firstSlide(product.images);
+            otherSlides(product.images);
         }
     });
 });
