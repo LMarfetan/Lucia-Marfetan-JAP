c955e69381ebc4b32a5cd283c9dd42047c6b5c69
LMarfetan <54559223+LMarfetan@users.noreply.github.com>
2019-10-06 09:22:58 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,4 @@
+var cart = [];
 let productUnitCost = 0;
 let productCurrency = "";
 let subtotal = 0;
@@ -25,13 +26,62 @@ function hidePaymentTypeNotSelected(){
 
 }
 
-function showArticles(articles){
+function showArticles(array){
 
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let article = array[i];
+        
+            htmlContentToAppend += `
+
+                <div class="row">
+                    <div class="col-3 border">
+                        <img src="` + article.src + `" alt="`+ article.name +`" class="img-thumbnail rounded mx-auto d-block">
+                    </div>
+                    <div class="col border">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ article.name +`</h4>
+                            <p class="text-muted">` + article.unitCost + ` ` + article.currency + `</small>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="input-group">
+                                <span class="input-group-btn input-group-prepend">
+                                    <button type="button" class="btn btn-dark btn-number" disabled="disabled" data-type="minus" data-field="quant[1]">
+                                        <i class="fa fa-minus"></i>
+                                    </button>
+                                </span>
+                    
+                                <input type="text" name="quant[1]" class="form-control input-number text-center" value="`+ article.count +`" min="1" max="10">
+
+                                <span class="input-group-btn input-group-append">
+                                    <button type="button" class="btn btn-dark btn-number" data-type="plus" data-field="quant[1]">
+                                        <i class="fa fa-plus"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           
+            `
+
+
+        }
+    document.getElementById("cart").innerHTML = htmlContentToAppend;
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+    document.addEventListener("DOMContentLoaded", function(e){
+        getJSONData(CART_INFO_URL).then(function(resultObj){
+            if (resultObj.status === "ok")
+            {
+                cart = resultObj.data; 
 
-});
+                showArticles(cart.articles);
+
+                
+            }
+        });
+    });
