4adfee4c82ac87d1150c77138d0a292ceb421bf5
LMarfetan <54559223+LMarfetan@users.noreply.github.com>
2019-10-26 22:19:33 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,11 @@
 var cart = [];
+let daysmax = 5;
+let daysmin = 2;
 let productCount = 0;
 let productUnitCost = 0;
 let productCurrency = "";
 let productCost = 0;
+let subtotalProduct = 0;
 let subtotal = 0;
 let shippingPercentage = 0.15;
 let total = 0;
@@ -14,19 +17,43 @@ let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts(){
     let totalHTML = document.getElementById("total");
-    let totalCurrencyHTML = document.getElementById("totalCurrency");
-    total = (subtotal*(1+shippingPercentage));
-    totalHTML.innerHTML = total;
-    totalCurrencyHTML = productCurrency;
+    total = (subtotal*(1+shippingPercentage)).toFixed(2);
+    
+    totalHTML.innerHTML = "Total: " + total +" "+ productCurrency;
 }
 
-function updateSubtotal(){
+function updateSubtotal(array){
+
     let subtotalHTML = document.getElementById("subtotal");
-    let subtotalCurrencyHTML = document.getElementById("subtotalCurrency");
-    subtotal = (productCount)*(productUnitCost);
-    subtotalHTML.innerHTML = subtotal;
-    subtotalCurrencyHTML = productCurrency;
+    let subtotalHTMLToAppendHTML = document.getElementById("subtotalHTMLToAppend")
+    
+    for(let i = 0; i < array.length; i++){
+        let article = array[i];
+
+        subtotalProduct = (productCount)*(article.unitCost);
+        subtotal =+ subtotalProduct;
+        subtotalHTMLToAppendHTML.innerHTML = subtotalProduct +" "+ productCurrency;
+    }
+    
+    subtotalHTML.innerHTML = "Subtotal: " + subtotal +" "+ productCurrency;
+    
 }
+
+
+function updateDate(){
+
+    var date = new Date();
+
+    let deliveryDateMinHTML = document.getElementById("deliveryDateMin"); 
+    let deliveryDateMaxHTML = document.getElementById("deliveryDateMax");
+        
+    var dd = date.getDate();
+    var mm = date.getMonth();
+    var yyyy = date.getFullYear();
+    
+
+}
+
 
 function showPaymentTypeNotSelected(){
 
@@ -40,59 +67,46 @@ function showArticles(array){
 
     let htmlContentToAppend = "";
     
-
     for(let i = 0; i < array.length; i++){
         let article = array[i];
 
         productUnitCost = article.unitCost;
         productCurrency = article.currency;
+        productCount = article.count
+        subtotalProduct = productCount*productUnitCost
         
-            htmlContentToAppend += `
-
-                
-                    <div class="col-md-9 border">
+            htmlContentToAppend += `                
+                    <div class="p-3 col-md-11 border">
                         <div class="row">
                             <div class="col">
                                 <img src="` + article.src + `" alt="`+ article.name +`" class="img-thumbnail rounded mx-auto d-block">
                             </div>
                         </div>
                         <div class="row mt-1">
-                            <div class="border col-lg-8">
+                            <div class="p-3 col-lg-8">
                                 <p class="font-weight-bold">`+ article.name +`</p>
+                                <small>Stock disponible</small> 
                             </div>
-                            <div class="border col-lg-4">
-                                <p class="font-weight-bold">` + article.unitCost + `` + article.currency + `</p>
+                            <div class="p-3 col-lg-4">
+                                <p class="font-weight-bold">` + article.unitCost + ` ` + article.currency + `</p>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col border" style="height: 100px"></div>
+                            <div class="col" style="height: 100px"></div>
                         </div>
                         <div class="row">
                             <div class="col-lg-8">
                                 <div class="input-group">
-                                    <span class="input-group-btn-prepend">
-                                        <button type="button" class="btn btn-primary btn-number" data-type="minus" data-field="quant[1]">
-                                            <img src="img/minus.png" style="width: 20px" alt="minus">
-                                        </button>
-                                    </span>
-
-                                    <input type="number" name="quant[1]" id="productCount" class="form-control input-number text-center" value="`+ article.count +`>
-
-                                    <span class="input-group-btn-append">
-                                        <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[1]">
-                                            <img src="img/plus.png" style="width: 20px" alt="plus">
-                                        </button>
-                                    </span>
+                                   <input type="number" id="productCount" class="form-control input-number text-center" value="`+ productCount +`">
                                 </div>
                             </div>
                             <div class="col-lg-4">
-                                <p class="font-weight-bold">`+ subtotal +``+ article.currency +`</p>
+                                <p id="subtotalHTMLToAppend" class="font-weight-bold">`+ subtotalProduct +``+ article.currency +`</p>
                             </div>
                         </div>
                     </div>
            
             `
-
         }
     document.getElementById("cart").innerHTML = htmlContentToAppend;
 }
@@ -106,12 +120,39 @@ function showArticles(array){
             {
                 cart = resultObj.data;
                 showArticles(cart.articles);
+                updateSubtotal(cart.articles);
+                updateTotalCosts();
+                updateDate();
             }
 
             document.getElementById("productCount").addEventListener("change", function(){
                 productCount = this.value;
-                updateSubtotal();
+                updateSubtotal(cart.articles);
                 updateTotalCosts();
+            });
+
+            document.getElementById("premiumradio").addEventListener("change", function(){
+                shippingPercentage = 0.15;
+                daysmin = 2;
+                daysmax = 5;
+                updateTotalCosts();
+                updateDate();
+            });
+            
+            document.getElementById("expressradio").addEventListener("change", function(){
+                shippingPercentage = 0.07;
+                daysmin = 5;
+                daysmax = 8;
+                updateTotalCosts();
+                updateDate();
+            });
+        
+            document.getElementById("standardradio").addEventListener("change", function(){
+                shippingPercentage = 0.05;
+                daysmin = 12;
+                daysmax = 15;
+                updateTotalCosts();
+                updateDate();
             });
         });
     });
